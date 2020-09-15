2b6d5ad9999c863888e1cb8aa31f4e920c9a9a93
Étienne Cloutier <etiennecloutier00@gmail.com>
2020-09-15 16:49:00 -0400

    first save EC

---
+++ portal0/Assets/Etienne Cloutier/Portals/Script/Portal.cs
@@ -0,0 +1,31 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public float maxSpeed = 7;
+
+    private Vector2 targetVelocity;
+    public GameObject preFabPortal;
+    private bool notdone = true;
+
+    void Start()
+    { 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetAxis("Fire1")!=0 && notdone)
+        {
+            Instantiate(preFabPortal, new Vector3(-3f, 1f, -3f), Quaternion.Euler(0, 90, 0));
+            notdone = false;
+        }
+
+    }
+  
+}

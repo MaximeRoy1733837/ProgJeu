6ec6ee291e6bc77448307c308e03b21b2d1d2fe7
MaximeRoy1733837 <maxroy14@hotmail.ca>
2020-09-22 16:25:09 -0400

    Shoot

---
+++ portal0/Assets/ScriptMax/Shoot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+
+    public float damage = 10f;
+    public float range = 100f;
+    public Camera cam;
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            ShootPortal(1);
+        }
+        if (Input.GetButtonDown("Fire2"))
+        {
+            ShootPortal(2);
+        }
+    }
+
+    void ShootPortal(int color)
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(cam.transform.position, cam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+        }
+
+    }
+}

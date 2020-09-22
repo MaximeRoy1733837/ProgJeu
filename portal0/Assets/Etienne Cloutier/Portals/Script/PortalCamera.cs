5cf7640870fd714d53aab598f78287396cbe29e1
Étienne Cloutier <etiennecloutier00@gmail.com>
2020-09-22 18:18:14 -0400

    creation portal auto 50%

---
+++ portal0/Assets/Etienne Cloutier/Portals/Script/PortalCamera.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalCamera : MonoBehaviour
+{
+    private Camera camera;
+
+    public Transform playerCamera;
+    public Transform portal;
+    public Transform otherPortal;
+
+    private Material cameraMaterial;
+
+    void Start()
+    {
+        cameraMaterial = new Material(Shader.Find("Standard"));
+        camera = GetComponent<Camera>();
+
+        playerCamera = FindObjectOfType<PlayerCamera>().transform;
+        otherPortal = transform;
+        otherPortal.position = transform.position * 2;
+
+        camera.targetTexture = new RenderTexture(Screen.width,Screen.height,24);
+        cameraMaterial.mainTexture = camera.targetTexture;
+        
+    }
+    void Update()
+    {
+        //portal cam position
+        Vector3 playerOffsetFromPortal = playerCamera.position - otherPortal.position;
+        transform.position = portal.position + playerOffsetFromPortal;
+        //portal cam rotation
+        float angularDifferenceBetweenPortalRotation = Quaternion.Angle(portal.rotation, otherPortal.rotation);
+        Quaternion portalRotationalDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortalRotation, Vector3.up);
+        Vector3 newCameraDirection = portalRotationalDifference * playerCamera.forward;
+        transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
+    }
+
+    public void UpdateOtherPortal(Transform aOtherPortal)
+    {
+        otherPortal = aOtherPortal;
+    }
+}
---
+++ portal0/Assets/Scenes/Player/Script/PortalManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalManager : MonoBehaviour
+{
+    public GameObject PortalPrefab;
+
+    private GameObject portalBlue;
+    private GameObject portalOrange;
+    private Camera camBlue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        portalBlue = Instantiate(PortalPrefab,transform.position,transform.rotation);
+        camBlue= portalBlue.GetComponent<Camera>();
+        /*for(int i=0;i<portalBlue.transform.childCount;i++)
+        {
+            Transform currentItem = portalBlue.transform.GetChild(i);
+            if(currentItem.name.Equals("Centre"))
+            {
+                currentItem.gameObject.GetComponent<Renderer>().material=camBlue.GetComponent<>
+            }
+        }*/
+        //portalBlue.GetComponent<PortalCenter>().GetComponent<Renderer>().material=
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

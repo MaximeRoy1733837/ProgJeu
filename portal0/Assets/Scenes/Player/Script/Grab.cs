b4a141fa94c2f36299dd7fc4b31929c242909213
Cl0ut1eR <etiennecloutier00@gmail.com>
2020-10-27 13:42:48 -0400

    Debut creation grabbable

---
+++ portal0/Assets/Scenes/Player/Script/Grab.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grab : MonoBehaviour
+{
+    public float grabDistance = 1f;
+    void Update()
+    {
+        
+        if(Input.GetKeyUp("e"))
+        {
+            PickUp();
+        }
+    }
+    void PickUp()
+    { }
+}

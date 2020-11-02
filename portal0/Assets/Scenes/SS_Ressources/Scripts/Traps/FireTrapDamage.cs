c67335c7fa7c4ccee262a55546ea39cb5aa40809
samsim97 <neiz2500@hotmail.ca>
2020-11-02 10:36:37 -0500

    Ajout du son pour le piège de feu

---
+++ portal0/Assets/Scenes/SS_Ressources/Scripts/Traps/FireTrapDamage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireTrapDamage : MonoBehaviour
+{
+    private float playerDamageCooldown = 0.5f;
+    private float playerDamageCooldownValue = 0.5f;
+    public float trapDamage = 30f;
+
+    void Update() {
+
+        playerDamageCooldownValue -= Time.deltaTime;
+
+    }
+
+    private void OnTriggerStay(Collider other) {
+
+        if (playerDamageCooldownValue <= 0) {
+            if (other.transform.tag == "Player") {
+
+                GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealthSystem>().Damage(trapDamage);
+                playerDamageCooldownValue = playerDamageCooldown;
+
+            }
+        }
+
+    }
+}

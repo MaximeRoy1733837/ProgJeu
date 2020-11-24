b0f5ea33533597bcb6f2193cd0d1d658d8e23c66
Thycker <nikki090298@hotmail.com>
2020-11-24 15:29:10 -0500

    NextLevel manque garder nombreVie d une scene a l autre

---
+++ portal0/Assets/SceneTransitionSound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTransitionSound : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource audioSource;
+        audioSource = GetComponent<AudioSource>();
+        DontDestroyOnLoad(gameObject);
+        audioSource.Play();
+        Destroy(gameObject,audioSource.clip.length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

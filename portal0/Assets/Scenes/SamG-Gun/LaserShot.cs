4845eccc81c39c3f19427e7550fe5aecf8c949cf
MSI\samga <samgauthier2@hotmail.com>
2020-12-06 21:36:14 -0500

    adding Bullet/laser

--- portal0/Assets/Scenes/SamG-Gun/LaserShot.cs
+++ portal0/Assets/Scenes/SamG-Gun/LaserShot.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class LaserShot : MonoBehaviour
 {
+    public Transform laserPos;
+
+    public  Transform LaserBeamPrefab;
+
+    public float shootForce = 90.0f;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    private void OnFireGun()
+    {
+        if (LaserBeamPrefab != null)
+        {
+            CreateAndFireBullet(laserPos);
+        }
+    }
+
+    private void CreateAndFireBullet(Transform startPos)
+    {
+        var laser = (Transform)Instantiate(LaserBeamPrefab, startPos.position, startPos.rotation);
+
+        laser.GetComponent<Rigidbody>().velocity = startPos.forward * shootForce;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+        {
+            OnFireGun();
+        }
     }
 }
